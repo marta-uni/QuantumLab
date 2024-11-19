17f25d23d0070d1a24eeb91c2fa91f963a2b4c23
marta-uni <marta.pastori@studenti.unitn.it>
2024-11-19 11:56:11 +0100

    Add files via upload

--- functions.py
+++ functions.py
@@ -1,21 +1,19 @@
+from matplotlib.collections import QuadMesh
 import numpy as np
-from scipy.signal import find_peaks, peak_widths
+from pandas.core.common import is_full_slice
+from scipy.signal import find_peaks
 import matplotlib.pyplot as plt
 
-
 def crop_to_min_max(time, laser_voltage, piezo_voltage):
-    '''Crops time and voltage readings in order to include just one
-     frequency sweep from the piezo.'''
 
     if len(piezo_voltage) == 0:
         return None  # Return None if piezo_voltage is empty
 
     # Find indices of minimum and maximum values
-    # Assuming there will be just one full sweep in the dataset
     min_index = np.argmin(piezo_voltage)
     max_index = np.argmax(piezo_voltage)
 
-    # Ensure the possibility to analyse "backwards" sweeps too
+    # Ensure min_index is before max_index
     start_index = min(min_index, max_index)
     end_index = max(min_index, max_index)
 
@@ -28,9 +26,6 @@ def crop_to_min_max(time, laser_voltage, piezo_voltage):
 
 
 def fit_piezo_line(time, piezo_voltage):
-    '''Converts timestamps in voltages on piezo.
-
-    Returns voltages from a linear interpolation of input data.'''
 
     if len(time) == 0 or len(piezo_voltage) == 0 or len(time) != len(piezo_voltage):
         return None  # Return None if the input arrays are empty or of different lengths
@@ -39,131 +34,129 @@ def fit_piezo_line(time, piezo_voltage):
     slope, intercept = np.polyfit(time, piezo_voltage, 1)
     piezo_fit = slope * time + intercept
 
-    return piezo_fit
+    return time, piezo_fit
 
 
 def peaks(piezo_voltage, laser_voltage):
-    '''
-    Finds peaks in readings from the photodiode.
 
-    Parameters:
+    # Use find_peaks with specified height and distance
+    peaks_indices, _ = find_peaks(laser_voltage, height=0.08, distance=400)
 
-    piezo_voltage: voltages on the piezo, should be the cleaned version (the output of fit_piezo_line)
-    laser_voltage: voltages on the photodiode
-
-    Returns: 
-
-    peaks_xvalues: voltage values on the piezo corresponding to detected peaks
-    peaks: detected peaks
-    scaled_widths: width of each peak in Volts
-    '''
-
-    peaks_indices, _ = find_peaks(laser_voltage, height=0.3, distance=10)
+    # Convert peaks_indices to an integer NumPy array
+    peaks_indices = np.array(peaks_indices, dtype=int)
+    print(peaks_indices)
+    # Extract peak values
     peaks = laser_voltage[peaks_indices]
     peaks_xvalues = piezo_voltage[peaks_indices]
 
-    widths = peak_widths(laser_voltage, peaks_indices, rel_height=0.5)
-    piezo_voltage_spacing = np.mean(np.diff(piezo_voltage))
-    scaled_widths = widths[0]*piezo_voltage_spacing
-
-    return peaks_xvalues, peaks, scaled_widths
-
-
-def fsr(xpeaks, ypeaks):
-    '''
-    Computes FSR (in volts) as the distance between the two TEM00 peaks. For now it just
-    checks that the separation is bigger than 1V'.
-    '''
-
-    # combine peaks with corrisponding x values and sort them in descending order
-    sorted_peaks = sorted(list(zip(xpeaks, ypeaks)),
-                          key=lambda x: x[1], reverse=True)
-
-    # assume that the distance between the two highest peaks is the fsr
-    # for now it has to be bigger than 1V
-    top_peaks = []
-    for peak in sorted_peaks:
-        if (len(top_peaks) >= 2):
-            break
-        if all(abs(peak[0] - tp[0]) >= 1 for tp in top_peaks):
-            top_peaks.append(peak)
-
-    if (len(top_peaks) == 2):
-        fsr_volt = np.abs(top_peaks[0][0] - top_peaks[1][0])
-        return fsr_volt
-    else:
-        return None
-
-
-def close_modes(x, expected_mode_distance):
-    '''
-    Returns a list of (voltage) separations between close peaks in our spectrum.
-    These will be separated by mode_distance / fsr = (2/pi) * arccos(1-L/R) - 1 .
-    The definition of "close" depends on expected_mode_distance (the upper bound
-    in voltage difference), which has to be expressed in V.
-    '''
-
-    result = []
-
-    # finding pairs of close adjacent peaks, these will be separated by
-    # mode_distance / fsr = (2/pi) * arccos(1-L/R) - 1
-    for i in range(len(x) - 1):
-        if ((x[i + 1] - x[i]) < expected_mode_distance):
-            result.append(x[i + 1] - x[i])
-
-    # average mode distance, in volts
-    return result
-
+    return peaks_xvalues, peaks
 
 
 def plot_voltage_vs_time(timestamps, volt_laser, volt_piezo, piezo_fitted, file_name):
     plt.figure(figsize=(12, 6))
     plt.plot(timestamps, volt_laser, label='Volt Laser', color='blue')
     plt.plot(timestamps, volt_piezo, label='Volt Piezo', color='red')
-    plt.plot(timestamps, piezo_fitted,
-             label='Volt Piezo fitline', color='green')
+    plt.plot(timestamps, piezo_fitted, label='Volt Piezo fitline', color='green')
     plt.xlabel('Time (s)')
     plt.ylabel('Voltage (V)')
     plt.title('Voltage data vs Time')
     plt.legend()
-    plt.grid()
     plt.xticks(rotation=45)
     plt.tight_layout()
     plt.savefig(file_name)
     # plt.show()  # Uncomment this if you want to display the plot
     plt.close()  # Close the figure to avoid displaying it in-line
 
-
-def plot_piezo_laser(piezo_fitted, volt_laser, xpeaks, ypeaks, file_name, width):
+def plot_piezo_laser(piezo_fitted, volt_laser, xpeaks, ypeaks, file_name):
     plt.figure(figsize=(12, 6))
-    plt.plot(piezo_fitted, volt_laser, label='Laser Intensity vs. Piezo volt',
-             color='green', marker='.', linestyle=None)
-    plt.hlines(ypeaks/2, xpeaks-width/2, xpeaks+width/2)
+    plt.plot(piezo_fitted, volt_laser, label='Laser Intensity vs. Piezo volt', color='green')
     plt.scatter(xpeaks, ypeaks, marker='x', label='Peak Values')
     plt.xlabel('Voltage Piezo (V)')
     plt.ylabel('Laser Intensity (V)')
     plt.title('Piezo Voltage vs Laser Voltage')
     plt.legend()
-    plt.grid()
     plt.xticks(rotation=45)
     plt.tight_layout()
     plt.savefig(file_name)
-    # plt.show()
+    #plt.show()
     plt.close()  # Close the figure to avoid displaying it
-
 
 def plot_calibrated_laser(xvalues_freq, volt_laser, file_name):
     plt.figure(figsize=(12, 6))
-    plt.plot(xvalues_freq, volt_laser,
-             label='Laser Intensity vs. freq', color='green')
-    plt.xlabel('relative freq values(THz)( offset was set at 780nm ~ 384 THz )')
+    plt.plot(xvalues_freq, volt_laser, label='Laser Intensity vs. freq', color='green')
+    plt.xlabel('relative freq values(Hz)( offset was set at 780nm ~ 384 THz )')
     plt.ylabel('Laser Intensity (V)')
     plt.title(' Laser Intensity (calibrated)')
     plt.legend()
+    plt.xticks(rotation=45)
+    plt.tight_layout()
+    plt.savefig(file_name)
+    #plt.show()
+    plt.close()  # Close the figure to avoid displaying it
+
+
+def save_to_csv(timestamps, volt_laser, volt_piezo, piezo_fitted, output_file):
+    """
+    Save the cropped and fitted data to a CSV file with the specified columns.
+
+    Parameters:
+    - timestamps: Array of timestamps
+    - volt_laser: Array of laser voltages
+    - volt_piezo: Array of piezo voltages
+    - piezo_fitted: Array of fitted piezo voltage values
+    - output_file: Path to the output CSV file
+    """
+
+    # Create a DataFrame with the data
+    data = {
+        'timestamp': timestamps,
+        'volt_laser': volt_laser,
+        'volt_piezo': volt_piezo,
+        'piezo_fitted': piezo_fitted
+    }
+
+    df = pd.DataFrame(data)
+
+    # Save the DataFrame to a CSV file
+    df.to_csv(output_file, index=False)
+    print(f"Data saved to {output_file}")
+
+def lin_quad_fits(x,y):
+    #perform linear fit
+    coeffs_1 = np.polyfit(x, y, 1)  # coeffs = [a, b, c] for y = ax^2 + bx + c
+    linear_fit = np.poly1d(coeffs_1)  # Create a polynomial function from the coefficients
+    # Perform quadratic fit (polynomial of degree 2)
+    coeffs_2 = np.polyfit(x, y, 2)  # coeffs = [a, b, c] for y = ax^2 + bx + c
+    quadratic_fit = np.poly1d(coeffs_2)  # Create a polynomial function from the coefficients
+
+    # Generate x values for the fitted curve (same x range as the original data)
+    x_fit = np.linspace(min(x), max(x), 100)  # Smooth line for plotting
+    lin_fit = linear_fit(x_fit)  # Calculate the fitted line
+    quad_fit = quadratic_fit(x_fit)  # Calculate corresponding y values for the fitted curve
+    return coeffs_1, coeffs_2, x_fit, lin_fit, quad_fit
+
+def plot_generic(x, y, x_label, y_label, file_name):
+    coeffs_1, coeffs_2, x_fit, lin_fit, quad_fit = lin_quad_fits(x, y)
+    
+    # Create the plot
+    plt.figure(figsize=(12, 6))
+    plt.scatter(x, y, label='Data', color='green')
+    plt.plot(x_fit, lin_fit, label='Linear Fit', color='blue', linestyle='--')  # Plot the quadratic fit
+    plt.plot(x_fit, quad_fit, label='Quadratic Fit', color='red', linestyle='--')  # Plot the quadratic fit
+    plt.xlabel(x_label)
+    plt.ylabel(y_label)
+    plt.title(x_label + ' vs ' + y_label)
+    plt.legend()
+    plt.xticks(rotation=45)
     plt.grid()
-    plt.ticklabel_format(style='sci', axis='x',
-                         scilimits=(12, 12), useOffset=False)
     plt.tight_layout()
     plt.savefig(file_name)
     # plt.show()
     plt.close()  # Close the figure to avoid displaying it
+
+    
+    #return coeffs_1
+
+
+
+
