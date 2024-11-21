e8b311ade8464ba50a34a904743eb821e816a821
Francesco Galasso <francesco.galasso7@studio.unibo.it>
2024-11-21 09:56:26 +0100

    first new commit, saving stuff

--- functions.py
+++ functions.py
@@ -1,6 +1,7 @@
 import numpy as np
 from scipy.signal import find_peaks, peak_widths
 import matplotlib.pyplot as plt
+import pandas as pd
 
 
 def crop_to_min_max(time, laser_voltage, piezo_voltage):
@@ -42,7 +43,7 @@ def fit_piezo_line(time, piezo_voltage):
     return piezo_fit
 
 
-def peaks(piezo_voltage, laser_voltage):
+def peaks(piezo_voltage, laser_voltage, height, distance):
     '''
     Finds peaks in readings from the photodiode.
 
@@ -50,6 +51,8 @@ def peaks(piezo_voltage, laser_voltage):
 
     piezo_voltage: voltages on the piezo, should be the cleaned version (the output of fit_piezo_line)
     laser_voltage: voltages on the photodiode
+    height: min height of the peaks
+    height: min number of point between a peak and the following one
 
     Returns: 
 
@@ -57,8 +60,8 @@ def peaks(piezo_voltage, laser_voltage):
     peaks: detected peaks
     scaled_widths: width of each peak in Volts
     '''
-
-    peaks_indices, _ = find_peaks(laser_voltage, height=0.3, distance=10)
+    peaks_indices, _ = find_peaks(
+        laser_voltage, height=height, distance=distance)
     peaks = laser_voltage[peaks_indices]
     peaks_xvalues = piezo_voltage[peaks_indices]
 
@@ -69,14 +72,14 @@ def peaks(piezo_voltage, laser_voltage):
     return peaks_xvalues, peaks, scaled_widths
 
 
-def fsr(xpeaks, ypeaks):
+def fsr(xpeaks, ypeaks, dx):
     '''
     Computes FSR (in volts) as the distance between the two TEM00 peaks. For now it just
-    checks that the separation is bigger than 1V'.
+    checks that the separation is bigger than 4V and smaller than 8V'.
     '''
 
     # combine peaks with corrisponding x values and sort them in descending order
-    sorted_peaks = sorted(list(zip(xpeaks, ypeaks)),
+    sorted_peaks = sorted(list(zip(xpeaks, ypeaks, dx)),
                           key=lambda x: x[1], reverse=True)
 
     # assume that the distance between the two highest peaks is the fsr
@@ -85,12 +88,13 @@ def fsr(xpeaks, ypeaks):
     for peak in sorted_peaks:
         if (len(top_peaks) >= 2):
             break
-        if all(abs(peak[0] - tp[0]) >= 1 for tp in top_peaks):
+        if all(4 <= abs(peak[0] - tp[0]) < 8 for tp in top_peaks):
             top_peaks.append(peak)
 
     if (len(top_peaks) == 2):
         fsr_volt = np.abs(top_peaks[0][0] - top_peaks[1][0])
-        return fsr_volt
+        d_fsr = np.sqrt((top_peaks[0][2] ** 2) + (top_peaks[1][2] ** 2))
+        return fsr_volt, d_fsr
     else:
         return None
 
@@ -115,55 +119,82 @@ def close_modes(x, expected_mode_distance):
     return result
 
 
+def even_odd_modes(x, y, dx, expected_mode_distance):
+    '''
+    Returns 2 lists of (voltage) separations between even and odd modes in our spectrum and a list with
+    errors on estimates.
+    These modes are ideantified via an upper (as parameter) and lower bound (1V) and the biggest one
+    being >0.4V.
+    list_1 has even -> odd separations
+    list_2 has odd -> even separations
+    '''
 
-def plot_voltage_vs_time(timestamps, volt_laser, volt_piezo, piezo_fitted, file_name):
-    plt.figure(figsize=(12, 6))
-    plt.plot(timestamps, volt_laser, label='Volt Laser', color='blue')
-    plt.plot(timestamps, volt_piezo, label='Volt Piezo', color='red')
-    plt.plot(timestamps, piezo_fitted,
-             label='Volt Piezo fitline', color='green')
-    plt.xlabel('Time (s)')
-    plt.ylabel('Voltage (V)')
-    plt.title('Voltage data vs Time')
-    plt.legend()
-    plt.grid()
-    plt.xticks(rotation=45)
-    plt.tight_layout()
-    plt.savefig(file_name)
-    # plt.show()  # Uncomment this if you want to display the plot
-    plt.close()  # Close the figure to avoid displaying it in-line
+    list_1 = []
+    list_2 = []
+    list_err_1 = []
+    list_err_2 = []
 
+    for i in range(len(x) - 1):
+        if (1 < (x[i + 1] - x[i]) < expected_mode_distance):
+            if (y[i + 1] < y[i]) and y[i] > 0.3:
+                list_1.append(x[i + 1] - x[i])
+                list_err_1.append(np.sqrt((dx[i + 1] ** 2) + (dx[i] ** 2)))
+            elif (y[i + 1] > 0.3):
+                list_2.append(x[i + 1] - x[i])
+                list_err_2.append(np.sqrt((dx[i + 1] ** 2) + (dx[i] ** 2)))
 
-def plot_piezo_laser(piezo_fitted, volt_laser, xpeaks, ypeaks, file_name, width):
-    plt.figure(figsize=(12, 6))
-    plt.plot(piezo_fitted, volt_laser, label='Laser Intensity vs. Piezo volt',
-             color='green', marker='.', linestyle=None)
-    plt.hlines(ypeaks/2, xpeaks-width/2, xpeaks+width/2)
-    plt.scatter(xpeaks, ypeaks, marker='x', label='Peak Values')
-    plt.xlabel('Voltage Piezo (V)')
-    plt.ylabel('Laser Intensity (V)')
-    plt.title('Piezo Voltage vs Laser Voltage')
-    plt.legend()
-    plt.grid()
-    plt.xticks(rotation=45)
-    plt.tight_layout()
-    plt.savefig(file_name)
-    # plt.show()
-    plt.close()  # Close the figure to avoid displaying it
+    return list_1, list_2, list_err_1, list_err_2
 
 
-def plot_calibrated_laser(xvalues_freq, volt_laser, file_name):
-    plt.figure(figsize=(12, 6))
-    plt.plot(xvalues_freq, volt_laser,
-             label='Laser Intensity vs. freq', color='green')
-    plt.xlabel('relative freq values(THz)( offset was set at 780nm ~ 384 THz )')
-    plt.ylabel('Laser Intensity (V)')
-    plt.title(' Laser Intensity (calibrated)')
-    plt.legend()
-    plt.grid()
-    plt.ticklabel_format(style='sci', axis='x',
-                         scilimits=(12, 12), useOffset=False)
-    plt.tight_layout()
-    plt.savefig(file_name)
-    # plt.show()
-    plt.close()  # Close the figure to avoid displaying it
+def weighted_avg(x, dx):
+    weights = 1 / dx**2
+
+    avg = np.sum(weights * x) / np.sum(weights)
+    std = np.sqrt(1 / np.sum(weights))
+
+    return avg, std
+
+
+def save_to_csv(timestamps, volt_laser, volt_piezo, piezo_fitted, output_file):
+    """
+    Save the cropped and fitted data to a CSV file with the specified columns.
+
+    Parameters:
+    - timestamps: Array of timestamps
+    - volt_laser: Array of laser voltages
+    - volt_piezo: Array of piezo voltages
+    - piezo_fitted: Array of fitted piezo voltage values
+    - output_file: Path to the output CSV file
+    """
+
+    # Create a DataFrame with the data
+    data = {
+        'timestamp': timestamps,
+        'volt_laser': volt_laser,
+        'volt_piezo': volt_piezo,
+        'piezo_fitted': piezo_fitted
+    }
+
+    df = pd.DataFrame(data)
+
+    # Save the DataFrame to a CSV file
+    df.to_csv(output_file, index=False)
+    print(f"Data saved to {output_file}")
+
+
+def lin_quad_fits(x, y):
+    # Perform linear fit
+    coeffs_1 = np.polyfit(x, y, 1)  # coeffs = [a, b] for y = ax + b
+    # Create a polynomial function from the coefficients
+    linear_fit = np.poly1d(coeffs_1)
+    # Perform quadratic fit
+    coeffs_2 = np.polyfit(x, y, 2)  # coeffs = [a, b, c] for y = ax^2 + bx + c
+    # Create a polynomial function from the coefficients
+    quadratic_fit = np.poly1d(coeffs_2)
+
+    # Generate x values for the fitted curve (same x range as the original data)
+    x_fit = np.linspace(min(x), max(x), 100)  # Smooth line for plotting
+    lin_fit = linear_fit(x_fit)  # Calculate the fitted line
+    # Calculate corresponding y values for the fitted curve
+    quad_fit = quadratic_fit(x_fit)
+    return coeffs_1, coeffs_2, x_fit, lin_fit, quad_fit
---
+++ renormalized_piezo.py
@@ -0,0 +1,102 @@
+import os
+import pandas as pd
+import plotting_functions as pf
+import functions2 as fn2
+import numpy as np
+
+c = 3e8
+l = 50e-3
+fsr_freq = c/(2*l)
+expected_wavelength = 780e-9
+
+'this code reads the csv files, crops them to a window where the piezo voltage is just increasing, removes NaNs and fits the piezo data linearly. It then saves a csv file with  4 columns: timestamps, volt_laser, volt_piezo, piezo_fitted'
+
+# Define the folder and file paths
+folder_name = 'data6'
+# Example: process files scope_15.csv to scope_25.csv
+file_paths = [
+    f"{folder_name}/clean_data/scope_{i}_cropped.csv" for i in range(15, 26)]
+
+# Loop through each file in the file_paths
+for file_path in file_paths:
+    file_name = os.path.splitext(file_path)[0].replace('_cropped', '')
+
+    '''prepare data (read, crop, fit piezo)'''
+
+    # Read the CSV file, skip the first 2 rows, and specify the data types
+    data = pd.read_csv(file_path, skiprows=2, names=['timestamp', 'volt_laser', 'volt_piezo', 'piezo_fitted'], dtype={
+                       'timestamp': float, 'volt_laser': float, 'volt_piezo': float, 'piezo_fitted': float})
+
+    timestamps = data['timestamp'].to_numpy()
+    volt_laser = data['volt_laser'].to_numpy()
+    volt_piezo = data['volt_piezo'].to_numpy()
+    piezo_fitted = data['piezo_fitted'].to_numpy()
+
+    # ASSUME CONFOCALITY
+
+    print(file_name)
+    print('Assuming confocality')
+
+    # Find peaks
+    # use this one to have 5 peaks (assume confocality within some precision)
+    xpeaks, ypeaks, indices = fn2.peaks_hfsr(piezo_fitted, volt_laser)
+
+    index = next((i for i, x in enumerate(xpeaks) if (
+        x >= -8) or (ypeaks[i] > 0.6)), len(xpeaks))
+    xpeaks = xpeaks[index:]
+    ypeaks = ypeaks[index:]
+    indices = indices[index:]
+
+    # Generate expected frequencies
+    expected_freq = np.arange(0, fsr_freq/2 * len(xpeaks), fsr_freq/2)
+
+    # find calibration, and plot the data
+    coeffs1, coeffs2 = fn2.plot_fits(xpeaks, expected_freq, "piezo_voltage_peaks",
+                                     "expected_frequency", file_name + "calibration-confocal")
+
+    # convert piezo voltages into frequencies
+    calibrated_freqs = coeffs2[0] * piezo_fitted**2 + \
+        coeffs2[1] * piezo_fitted + coeffs2[2]
+
+    pf.plot_generic(calibrated_freqs, volt_laser, "calibrated_freqs",
+                    "volt_laser", file_name + "calibrated_data_confocal")
+
+    peak_freq = calibrated_freqs[indices]
+
+    # displacement of the odd peaks from half the fsr (in Hz)
+    displacement1 = (peak_freq[1] - peak_freq[0]) - \
+        (peak_freq[2] - peak_freq[0]) / 2
+    displacement2 = (peak_freq[3] - peak_freq[2]) - \
+        (peak_freq[4] - peak_freq[2]) / 2
+
+    print(
+        f'Displacements from confocality are:\t{displacement1/1e6:.0f} MHz\t{displacement1/1e6:.0f} MHz\n')
+
+    #DONT ASSUME CONFOCALITY
+    print('Not assuming confocality')
+    
+    x_nonconfoc = xpeaks[::2]
+    y_nonconfoc = ypeaks[::2]
+    i_nonconfoc = indices[::2]
+
+    #generate expected frequencies
+    expected_freq = np.arange(0, fsr_freq * len(x_nonconfoc), fsr_freq)
+
+    #find calibration, and plot the data
+    coeffs1, coeffs2 = fn2.plot_fits(x_nonconfoc, expected_freq, "piezo_voltage_peaks", "expected_frequency", file_name + "calibration-confocal")
+
+    #convert piezo voltages into frequencies
+    calibrated_freqs = coeffs2[0] * piezo_fitted**2 + coeffs2[1] *piezo_fitted + coeffs2[2]
+
+    pf.plot_generic(calibrated_freqs, volt_laser, "calibrated_freqs", "volt_laser", file_name + "calibrated_data-nonconfocal")
+
+    peak_freq = calibrated_freqs[indices]
+
+    # displacement of the odd peaks from half the fsr (in Hz)
+    displacement1 = (peak_freq[1] - peak_freq[0]) - \
+        (peak_freq[2] - peak_freq[0]) / 2
+    displacement2 = (peak_freq[3] - peak_freq[2]) - \
+        (peak_freq[4] - peak_freq[2]) / 2
+
+    print(
+        f'Displacements from confocality are:\t{displacement1/1e6:.0f} MHz\t{displacement1/1e6:.0f} MHz\n')
