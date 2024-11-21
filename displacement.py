f3feb3265f488f3ce8b7024501915bc27ac675cc
Francesco Galasso <francesco.galasso7@studio.unibo.it>
2024-11-21 19:18:25 +0100

    displacement calculations

---
+++ displacement.py
@@ -0,0 +1,99 @@
+import pandas as pd
+import plotting_functions as pf
+from scipy.signal import find_peaks
+import fit_peaks as fp
+import numpy as np
+import functions as fn
+
+def odd_peaks(x, y, fig=False, figname='odd.pdf', save=False):
+    lor, cov = fp.fit_peaks(x, y, height=0.042, distance=80)
+
+    if fig:
+        xpeaks, ypeaks, peak_widths = fn.peaks(
+            x, y, height=0.042, distance=80)
+        A_list = []
+        x0_list = []
+        gamma_list = []
+
+        for popt in lor:
+            A_list.append(popt[0])
+            gamma_list.append(popt[2])
+            x0_list.append(popt[1])
+
+        pf.plot_freq_laser_fit(x, y, file_name=figname, A=A_list,
+                               x0=x0_list, gamma=gamma_list, xpeaks=xpeaks, ypeaks=ypeaks, width=peak_widths, save=save)
+
+    close_peaks = lor[1][1] - lor[0][1]
+    d_close = np.sqrt(cov[1][1, 1] + cov[1][1, 1])
+
+    return close_peaks, d_close
+
+
+for i in range(15, 26):
+    file_name = "scope_" + str(i)
+    folder_name = "data6/clean_data"
+    file_path = folder_name + "/" + file_name + "_calib.csv"
+    print(file_name)
+
+    data = pd.read_csv(
+        file_path,
+        skiprows=2,
+        names=["freq_confoc", "freq_non_confoc", "volt_laser"],
+        dtype={"freq_confoc": float,
+               "freq_non_confoc": float, "volt_laser": float},
+    )
+
+    freq_confoc = data["freq_confoc"].to_numpy()
+    freq_non_confoc = data["freq_non_confoc"].to_numpy()
+    volt_laser = data["volt_laser"].to_numpy()
+
+    peak_indices, _ = find_peaks(volt_laser, height=0.08, distance=400)
+    index = next(
+        (i for i, x in enumerate(peak_indices) if freq_confoc[x] > -5e8),
+        len(peak_indices),
+    )
+    peak_indices = peak_indices[index:]
+
+    peaks_confoc = freq_confoc[peak_indices]
+    peaks_non_confoc = freq_non_confoc[peak_indices]
+
+    displacement_confoc_1 = peaks_confoc[1] - \
+        peaks_confoc[0] - (peaks_confoc[2] - peaks_confoc[0]) / 2
+
+    displacement_confoc_2 = peaks_confoc[3] - \
+        peaks_confoc[2] - (peaks_confoc[4] - peaks_confoc[2]) / 2
+
+    displacement_non_confoc_1 = peaks_non_confoc[1] - peaks_non_confoc[0] - (
+        peaks_non_confoc[2] - peaks_non_confoc[0]) / 2
+
+    displacement_non_confoc_2 = peaks_non_confoc[3] - peaks_non_confoc[2] - (
+        peaks_non_confoc[4] - peaks_non_confoc[2]) / 2
+
+    print("Simple displacement estimation: frequency separation between odd modes and half the fsr.")
+    print(
+        f"Assuming confocality:\t{displacement_confoc_1/1e6:.0f} MHz\t{displacement_confoc_2/1e6:.0f} MHz")
+    print(
+        f"Without assuming confocality:\t{displacement_non_confoc_1/1e6:.0f} MHz\t{displacement_non_confoc_2/1e6:.0f} MHz")
+
+    close_peaks_confoc_1, d_close_confoc_1 = odd_peaks(
+        freq_confoc[peak_indices[1] - 600: peak_indices[1] + 200], volt_laser[peak_indices[1] - 600: peak_indices[1] + 200])
+
+    close_peaks_confoc_2, d_close_confoc_2 = odd_peaks(
+        freq_confoc[peak_indices[3] - 600: peak_indices[3] + 200], volt_laser[peak_indices[3] - 600: peak_indices[3] + 200])
+
+    close_peaks_non_confoc_1, d_close_non_confoc_1 = odd_peaks(
+        freq_non_confoc[peak_indices[1] - 600: peak_indices[1] + 200], volt_laser[peak_indices[1] - 600: peak_indices[1] + 200])
+
+    close_peaks_non_confoc_2, d_close_non_confoc_2 = odd_peaks(
+        freq_non_confoc[peak_indices[3] - 600: peak_indices[3] + 200], volt_laser[peak_indices[3] - 600: peak_indices[3] + 200])
+
+    # pf.plot_calibrated_laser(freq_confoc[peak_indices[3] - 600: peak_indices[3] + 200],
+    #                          volt_laser[peak_indices[3] - 600: peak_indices[3] + 200], "name")
+
+    print('Close peaks separation in odd modes:')
+    print(
+        f'Assuming confocality:\t{close_peaks_confoc_1/1e6:.2f} +/- {d_close_confoc_1/1e6:.2f} MHz' +
+        f'\t{close_peaks_confoc_2/1e6:.2f} +/- {d_close_confoc_2/1e6:.2f} MHz')
+    print(
+        f'Without assuming confocality:\t{close_peaks_non_confoc_1/1e6:.2f} +/- {d_close_non_confoc_1/1e6:.2f} MHz' +
+        f'\t{close_peaks_non_confoc_2/1e6:.2f} +/- {d_close_non_confoc_2/1e6:.2f} MHz\n')
