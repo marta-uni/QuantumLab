62c23253f35a5526e619450d901bb5a270b4a6b5
Francesco Galasso <francesco.galasso7@studio.unibo.it>
2024-11-30 22:43:30 +0100

    producing new pictures for calibration

--- spectroscopy_temperature.py
+++ spectroscopy_temperature.py
@@ -8,35 +8,35 @@ Rb = [377104390084020.94, 377104798412020.94, 377105206740020.94, 37710590987848
 Rb_labels = ['21', 'cross', '22', '32', 'cross', '33', '22', 'cross', '23', '11', 'cross', '12']
 
 #load data + plot
-const_i_filename = "data/Fixed_ld00000.csv"
+const_i_filename = "data9/clean_data/Fixed_ld00000.csv"
 const_i_data = pd.read_csv(const_i_filename, skiprows=1, names=['timestamp', 'photodiode', 'volt_piezo', 'volt_ld'], dtype={'timestamp': float, 'photodiode': float, 'volt_piezo': float, 'volt_ld': float})
-fn.plotting3(const_i_data['timestamp'], const_i_data['photodiode'], const_i_data['volt_piezo']/10, const_i_data['volt_ld'], 'time', 'pd', 'piezo/10', 'ld', 'constant diode current, modulated piezo position', 'figures_temperature/time_constdiode.pdf', save = True)
+fn.plotting3(const_i_data['timestamp'], const_i_data['photodiode'], const_i_data['volt_piezo']/10, const_i_data['volt_ld'], 'time', 'pd', 'piezo/10', 'ld', 'constant diode current, modulated piezo position', 'data9/figures/temperature/time_constdiode.pdf', save = True)
 
 #load peaks
-const_i_peaks_filename = "data/Fixed_ld00000_peaks (1).csv"
-const_i_peaks = pd.read_csv(const_i_peaks_filename, skiprows=1, names=['indices','timestamp', 'pd_peaks','piezo_peaks','ld_peaks','freq', 'lor_mean', 'lor_A', 'lor_gamma'])
+const_i_peaks_filename = "data9/clean_data/Fixed_ld00000_peaks_fit.csv"
+const_i_peaks = pd.read_csv(const_i_peaks_filename)
 
 #fitting peaks data to find freq conversions: dv/dVp (at const diode current) and dv/dt (v = nu)
 #if we want to use lorentzian, the one in piezovolt doesnt work anymore
-const_i_coeff1, const_i_coeff2 = fn.plot_fits(const_i_peaks['piezo_peaks'], const_i_peaks['freq'], 'piezo_peaks', 'freq', 'Constant diode current, modulated piezo position', "figures_temperature/fit_constdiode.pdf", save=True)
-dv_dVp = const_i_coeff1[0]
-const_i_freq_volt = dv_dVp * const_i_data['volt_piezo'] + const_i_coeff1[1]
-fn.plotting(const_i_freq_volt, const_i_data['photodiode'],'calibrated frequency', 'signal at photodiode', 'spectroscopy of Rb, \n constant diode current, modulated piezo position', "figures_temperature/spec_constdiode_volt.pdf", save=True)
+const_i_coeff1, const_i_coeff2 = fn.plot_fits(const_i_peaks['piezo_peaks'], const_i_peaks['freq'], 'piezo_peaks', 'freq', 'Constant diode current, modulated piezo position', "data9/figures/temperature/fit_constdiode.pdf", save=True)
+df_dVp = const_i_coeff1[0]
+const_i_freq_volt = df_dVp * const_i_data['volt_piezo'] + const_i_coeff1[1]
+fn.plotting(const_i_freq_volt, const_i_data['photodiode'],'calibrated frequency', 'signal at photodiode', 'spectroscopy of Rb, \n constant diode current, modulated piezo position', "data9/figures/temperature/spec_constdiode_volt.pdf", save=True)
 
 
-t_const_i_coeff1, t_const_i_coeff2 = fn.plot_fits(const_i_peaks['timestamps'], const_i_peaks['freq'], 'time', 'freq', 'Fixed Piezo, modulated diode current: time calib', "figures_temperature/timefit_constdiode.pdf", save=True)
-dv_dt = t_const_i_coeff1[0]
-const_i_freq_time = dv_dt * const_i_data['timestamp'] + t_const_i_coeff1[1]
-fn.plotting(const_i_freq_time, const_i_data['photodiode'],'calibrated frequency', 'signal at photodiode', 'spectroscopy of Rb, \n constant diode current, modulated piezo position', "figures_temperature/spec_constdiode_time.pdf", save=True)
+t_const_i_coeff1, t_const_i_coeff2 = fn.plot_fits(const_i_peaks['timestamp'], const_i_peaks['freq'], 'time', 'freq', 'Fixed Piezo, modulated diode current: time calib', "data9/figures/temperature/timefit_constdiode.pdf", save=True)
+df_dt = t_const_i_coeff1[0]
+const_i_freq_time = df_dt * const_i_data['timestamp'] + t_const_i_coeff1[1]
+fn.plotting(const_i_freq_time, const_i_data['photodiode'],'calibrated frequency', 'signal at photodiode', 'spectroscopy of Rb, \n constant diode current, modulated piezo position', "data9/figures/temperature/spec_constdiode_time.pdf", save=True)
 
 #comparison:
-fn.plotting(const_i_data['photodiode'], const_i_freq_time - const_i_freq_volt, 'signal at photodiode','calibrated frequency', 'spectroscopy of Rb, \n constant diode current, modulated piezo position', "figures_temperature/spec_constdiode_diff.pdf", save=True)
+fn.plotting(const_i_data['photodiode'], const_i_freq_time - const_i_freq_volt, 'signal at photodiode','calibrated frequency', 'spectroscopy of Rb, \n constant diode current, modulated piezo position', "data9/figures/temperature/spec_constdiode_diff.pdf", save=True)
 
 #conclusion: calibrating with time or with piezo voltage doesnt change anything
 
 #so now:
 
-t_const_i_coeff1, t_const_i_coeff2 = fn.plot_fits(const_i_peaks['lor_mean'], const_i_peaks['freq'], 'time', 'freq', 'Fixed Piezo, modulated diode current: time calib', "figures_temperature/timefit_constdiode.pdf", save=True)
-dv_dt = t_const_i_coeff1[0]
-const_i_freq_time = dv_dt * const_i_data['timestamp'] + t_const_i_coeff1[1]
+t_const_i_coeff1, t_const_i_coeff2 = fn.plot_fits(const_i_peaks['lor_mean'], const_i_peaks['freq'], 'time', 'freq', 'Fixed Piezo, modulated diode current: time calib', "data9/figures/temperature/timefit_constdiode.pdf", save=True)
+df_dt = t_const_i_coeff1[0]
+const_i_freq_time = df_dt * const_i_data['timestamp'] + t_const_i_coeff1[1]
 
