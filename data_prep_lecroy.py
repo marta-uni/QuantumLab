3689f3a530b9f3900d3efbdc2b70ac340b7a7280
Francesco Galasso <francesco.galasso7@studio.unibo.it>
2024-12-06 17:05:30 +0100

    data10

--- data_prep_lecroy.py
+++ data_prep_lecroy.py
@@ -9,14 +9,15 @@ removes NaNs and fits the piezo data linearly. It then saves a csv file with  4 
 volt_laser, volt_piezo, piezo_fitted.'''
 
 # Define the folder and file paths
-folder_name = 'data9'
-title = 'fullspec00000'
+folder_name = 'data10'
+title = 'no_absorption00000'
 df = pd.DataFrame()
 # Import single channel files
 channel_files = [f'{folder_name}/C{i}{title}.csv' for i in range(1, 4)]
 # Define channel names
 column_titles = ['volt_laser', 'volt_piezo', 'volt_ld']
 os.makedirs(f"{folder_name}/clean_data", exist_ok=True)
+os.makedirs(f"{folder_name}/figures/simple_plots_time", exist_ok=True)
 print(title)
 
 # Loop through each file in the file_paths
@@ -45,6 +46,13 @@ volt_laser = df['volt_laser'].to_numpy()
 volt_piezo = df['volt_piezo'].to_numpy()
 volt_ld = df['volt_ld'].to_numpy()
 
+# Use it in case of mode hopping
+'''mask = (timestamps >= -0.01)
+timestamps = timestamps[mask]
+volt_laser = volt_laser[mask]
+volt_piezo = volt_piezo[mask]
+volt_ld = volt_ld[mask]'''
+
 # Cropping data to a single sweep
 # timestamps, volt_laser, volt_piezo, volt_ld = fn.crop_to_min_max(
 #     timestamps, volt_laser, volt_piezo, volt_ld)
@@ -59,7 +67,7 @@ ld_fitted = fn.fit_piezo_line(timestamps, volt_ld)
 plt.figure()
 plt.plot(timestamps, volt_laser, label='Laser intensity',
          color='blue', markersize=5, marker='.')
-plt.plot(timestamps, piezo_fitted, label='Piezo voltage',
+plt.plot(timestamps, piezo_fitted/10, label='Piezo voltage/10',
          color='red', markersize=5, marker='.')
 plt.plot(timestamps, volt_ld, label='Diode modulation',
          color='green', markersize=5, marker='.')
@@ -68,6 +76,7 @@ plt.ylabel('Channel Value [V]')
 plt.title('Timestamp vs Channel Data')
 plt.grid()
 plt.legend()
+plt.tight_layout()
 plt.savefig(f'{folder_name}/figures/simple_plots_time/{title}.pdf')
 plt.show()
 
@@ -79,5 +88,5 @@ df['volt_laser'] = volt_laser
 df['volt_piezo'] = piezo_fitted
 df['volt_ld'] = ld_fitted
 
-'''df.to_csv(output_file, index=False)
-print(f"Data saved to {output_file}")'''
+df.to_csv(output_file, index=False)
+print(f"Data saved to {output_file}")
